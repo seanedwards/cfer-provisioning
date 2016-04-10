5b34637e81b2a66181e552bee1f6c89a6f9067d8
Sean Edwards <stedwards87+git@gmail.com>
2016-04-10 14:57:21 -0400

    Initial commit of cfer-provisoning

---
+++ lib/cfer/provisioning.rb
@@ -0,0 +1,22 @@
+require "cfer/provisioning/version"
+require 'cfer'
+
+require 'base64'
+require 'yaml'
+
+module Cfer
+  module Provisioning
+  end
+end
+
+require 'cfer/provisioning/cfn-bootstrap'
+require 'cfer/provisioning/chef'
+
+Cfer::Core::Resource.extend_resource "AWS::EC2::Instance" do
+  include Cfer::Provisioning
+end
+
+Cfer::Core::Resource.extend_resource "AWS::AutoScaling::LaunchConfiguration" do
+  include Cfer::Provisioning
+end
+
---
+++ lib/cfer/provisioning/version.rb
@@ -0,0 +1,5 @@
+module Cfer
+  module Provisioning
+    VERSION = "0.1.0"
+  end
+end
